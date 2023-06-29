66f546bb86dbc8b3521f231dc44be43297d8256d
Russell <rwjk16@gmail.com>
2023-06-29 16:15:57 -0600

    Initial Commit

---
+++ FetchInterview/FetchInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchInterviewApp.swift
+//  FetchInterview
+//
+//  Created by Russell Weber on 2023-06-29.
+//
+
+import SwiftUI
+
+@main
+struct FetchInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
